a1df542cd2b157a873a498829a73fdbd785ebc46
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2026-08-02 18:08:43 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 const config = defineConfig({
   test: {
-    include: ["src/tests/**/*.integration.test.ts"],
+    include: ["src/tests/**/*.test.ts"],
   },
 });
 
